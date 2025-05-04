239799fbfa184885f6421d1621d2122dfde6c623
ADARSH KUMAR GUPTA <106335949+AdarshGzz@users.noreply.github.com>
2025-05-04 16:58:37 +0530

    seperated folder

---
+++ Pattern Question/numberIncreasingReversePyramid.java
@@ -0,0 +1,11 @@
+public class numberIncreasingReversePyramid {
+    public static void main(String[] args){
+        int n = 7;
+        for(int i=n;i>0;i--){
+            for(int j=1;j<=i;j++){
+                System.out.print(" "+j);
+            }
+            System.out.println();
+        }
+    }
+}
--- Pattern Question/numberTriangle.java
+++ Pattern Question/numberTriangle.java
@@ -6,8 +6,7 @@ public class numberTriangle {
                 System.out.print(" ");
             }
             for(int j=1;j<=i;j++){
-                System.out.print(" ");
-                System.out.print(i);
+                System.out.print(" "+i);
             }
             System.out.println();
         }
