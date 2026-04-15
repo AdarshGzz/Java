3268076acf8f55507f9d684cd3f4846efaf69cdf
ADARSH KUMAR GUPTA <106335949+AdarshGzz@users.noreply.github.com>
2026-04-16 01:50:52 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.java
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.java
@@ -14,31 +14,14 @@
  * }
  */
 class Solution {
-    public void inorder(List<TreeNode> list,TreeNode root){
-        if(root==null) return;
-        inorder(list,root.left);
-        list.add(root);
-        inorder(list,root.right);
+    public boolean dfs(HashSet<Integer> h,TreeNode root, int k){
+        if(root==null) return false;
+        if(h.contains(k-root.val)) return true;
+        h.add(root.val);
+        return dfs(h,root.left,k) || dfs(h,root.right,k);
     }
     public boolean findTarget(TreeNode root, int k) {
-        if(root==null) return false;
-        List<TreeNode> list = new ArrayList<>();
-        inorder( list, root);
-
-        int i=0;
-        int j= list.size()-1;
-        while(i<j){
-            int sum  = (list.get(i)).val + (list.get(j)).val;
-            if(sum==k){
-                return true;
-            }else if(sum<k){
-                i++;
-            }else{
-                j--;
-            }
-        }
-
-        return false;
-
+        HashSet<Integer> h = new HashSet<>();
+        return dfs(h,root,k);
     }
 }
