057c72c4a58bd29b852dc609a4df7a416f1924b9
ADARSH KUMAR GUPTA <106335949+AdarshGzz@users.noreply.github.com>
2026-07-02 17:33:59 +0530

    Time: 1 ms (59.82%), Space: 49 MB (9.4%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,67 +1,26 @@
 class Solution {
     public int trap(int[] height) {
-        // int n = height.length;
-        // int totalWater = 0 ;
-
-        // for(int i=0;i<n;i++){
-        //     int leftMax = 0;
-        //     int rightMax = 0;
-
-        //     for(int j =0;j<=i;j++){
-        //         leftMax = Math.max(leftMax,height[j]);
-        //     }
-
-        //     for(int j=i;j<n;j++){
-        //         rightMax = Math.max(rightMax,height[j]);
-        //     }
-
-        //     totalWater += Math.min(leftMax,rightMax) - height[i];
-        // } 
-        // return totalWater;
-        // ______
-
-        // int n = height.length;
-        // int left = 0;
-        // int right = n-1;
-
-        // int leftMax = height[left], rightMax=height[right];
-        // int water = 0;
-
-        // while(left<right){
-        //     if(leftMax<rightMax){
-        //         left++;
-        //         leftMax  = Math.max(leftMax,height[left]);
-        //         water += leftMax - height[left];
-        //     }else{
-        //         right--;
-        //         rightMax = Math.max(rightMax,height[right]);
-        //         water += rightMax -height[right];
-        //     }
-        // }
-
-        // return water;
-
         int n = height.length;
-        int l = 0;
-        int r = n-1;
-        int lmax = 0;
-        int rmax = 0;
         int water = 0;
-        while(l<r){
-            lmax = Math.max(lmax,height[l]);
-            rmax = Math.max(rmax,height[r]);
+        int[] lmax = new int[n];
+        lmax[0] = height[0];
+        int[] rmax = new int[n];
+        rmax[n-1] = height[n-1];
 
-            if(lmax<rmax){
-                water += lmax - height[l];
-                l++; 
-            }else{
-                water+= rmax - height[r];
-                r--;
-            }
+        for(int i=1;i<n;i++){
+            lmax[i] = Math.max(height[i],lmax[i-1]);
+        }
 
+        for(int i=n-2;i>=0;i--){
+            rmax[i] = Math.max(height[i],rmax[i+1]);
+        }
+
+        int l = 0;
+        while(l<n){
+            water += Math.min(lmax[l],rmax[l]) - height[l];
+            l++;
         }
 
         return water;
-
     }
 }
